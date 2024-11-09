1e5ab7b4828f39e844071fbf18818d8fe3609ade
Mephisto <38382271+NotMephisto@users.noreply.github.com>
2024-11-09 16:58:15 +0300

    Update theme.js
    change link

--- Comfy/theme.js
+++ Comfy/theme.js
@@ -1,6 +1,6 @@
 (() => {
 	const themeScript = document.createElement("SCRIPT");
 	themeScript.setAttribute("type", "text/javascript");
-	themeScript.setAttribute("src", "https://raw.githubusercontent.com/NotMephisto/Comfy-matugen/refs/heads/main/Comfy/theme.script.js");
+	themeScript.setAttribute("src", "https://notmephisto.github.io/Comfy-matugen/Comfy/theme.script.js");
 	document.head.appendChild(themeScript);
 })();
